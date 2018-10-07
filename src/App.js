b845676004551bba56afb1422c56c443688ab7b3
Muhammad Zeeshan Khan <zeeshankhanaptech@gmail.com>
2018-10-07 23:56:37 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-
+import './App.css'
 
 // import FbImageLibrary from 'react-fb-image-grid'
 import Facebook_Post from './Components/Facebook_Post/Facebook_Post';
@@ -13,11 +13,11 @@ class App extends Component {
       { 
        fb_post : {
           createdBy: 'zeeshan',
-          avatar: 'https://www.facebook.com/photo.php?fbid=976215022570887&set=a.100620620130336&type=3&theater',
+          avatar: 'https://scontent.fkhi12-1.fna.fbcdn.net/v/t1.0-9/38875568_976215029237553_6527897523057590272_n.jpg?_nc_cat=105&oh=acbcbf58befacea5561035d08caa20f0&oe=5C536B42',
           description: "I'm not feeling good today!",
           images:
 ['https://goo.gl/JNcoRS','https://goo.gl/fsbwrh','https://goo.gl/U3X1gB','https://goo.gl/JNcoRS','https://goo.gl/fsbwrh','https://goo.gl/U3X1gB'],
-        createdAt: Date.now(),
+        createdAt:"Today 12:00 AM",
         likes: ['zeeshan','osama','owais','manal','kashif']
         }
     }
--- src/Components/Facebook_Post/Facebook_Post.js
+++ src/Components/Facebook_Post/Facebook_Post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import '../../App.css'
+
 
 import FacebookEmoji from 'react-facebook-emoji';
 import FbImageLibrary from 'react-fb-image-grid';
@@ -10,6 +10,9 @@ class Facebook_Post extends Component {
   constructor()
   {
     super();
+    this.state={
+      show:false
+    }
 
   }
   header()
@@ -21,32 +24,60 @@ class Facebook_Post extends Component {
   imagess()
   {
     const {fb_post} =this.props;
-  return  <div className="row"> <FbImageLibrary images={fb_post.images} countFrom={4} width={50} /></div>
+  return  <div className="row"></div>
     
   }
   mouseEnter()
   {
-    return(
    
-  
-  
-   
-    )
+    return(<div style={{marginTop:-40,position:"absolute"}}>
+    <FacebookEmoji type="like" size="sm"/>
+    <FacebookEmoji type="love" size="sm"/>
+    <FacebookEmoji type="wow" size="sm"/>
+    <FacebookEmoji type="yay" size="sm"/>
+    <FacebookEmoji type="angry" size="sm"/>
+    <FacebookEmoji type="haha" size="sm"/>
+    <FacebookEmoji type="sad" size="sm"/>
+</div>)
   
     
   }
   render() {
+    const {show}=this.state;
     const {fb_post} =this.props;
 
     return (
 
-      <div className="App">
+      <div className="container">
+      <div className="row" style={{marginBottom:10}} >
+      <div className="col-md-1"style={{textAlign:"left"}} >
+      <img className="img-circle"  src={fb_post.avatar} style={{borderRadius:100,width:50,height:50}}/>
+      </div>
+      <div className="col-md-2" style={{textAlign:"left"}}>
 
+      <p>{fb_post.createdBy}</p>
+     <p>
+      {fb_post.createdAt}
+      </p>
+    
+
+      </div>
+      <div className="col-md-9">
+      </div>
+   
+</div>
+          <div className="row" >
+
+          <FbImageLibrary images={fb_post.images} countFrom={4} />
+          </div>
           <div className="row">
-          <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" onMouseOver={()=>this.mouseEnter()} class="btn btn-secondary">Like</button>
-  <button type="button" class="btn btn-secondary">Comment</button>
-  <button type="button" class="btn btn-secondary">Share</button>
+          <div class="col-md-12" >
+          <div className="col-md-4 ">
+          {show && this.mouseEnter()}
+  <button type="button" onMouseOver={()=>this.mouseEnter()} class="btn btn-lg btn-default btn-block">Like</button>
+  </div>
+  <button type="button" class="col-md-4 btn btn-lg btn-default btn-block">Comment</button>
+  <button type="button"  style={{marginTop:0}} class="col-md-4 btn btn-lg btn-default btn-block">Share</button>
 </div>
           </div>
       </div>
